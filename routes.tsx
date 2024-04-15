1dccf450ad58597d2f55ca28c41d42ff1239a050
isaqkee <141652864+isaqkee@users.noreply.github.com>
2024-04-15 17:04:32 -0300

    CONSEGUI ADICIONAR AS 4
    HAHAHAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHAAAAAAAAAAAAAAAAAAAAAAAA

--- routes.tsx
+++ routes.tsx
@@ -1,6 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from './pages/receita'
 import { treinoAcademia } from './pages/treino'
+import { petShop } from './pages/auau'
+import { passagemBiblia } from './pages/biblia'
 import { Ionicons } from '@expo/vector-icons/'
 
 const Tab = createBottomTabNavigator();
@@ -36,6 +38,34 @@ export function Routes() {
                     }
                 }}
             />
+            <Tab.Screen
+                name="auau"
+                component={petShop}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return (<Ionicons size={25} color={"#000"} name="fish-outline" />)
+                        }
+                        return (<Ionicons size={20} color={"#000"} name="fish-outline" />)
+                    }
+                }}
+                />
+                 <Tab.Screen
+                name="biblia"
+                component={passagemBiblia}
+                options={{
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return (<Ionicons size={25} color={"#000"} name="book-outline" />)
+                        }
+                        return (<Ionicons size={20} color={"#000"} name="book-outline" />)
+                    }
+                }}
+                />
         </Tab.Navigator>
     )
 }
